7acfa32026831aaa61e1f82618c5f3792e09bb96
eirblaze <eirblaze@live.jp>
2022-12-07 18:05:29 +0900

    ライブラリモード解除

--- src/ts/webpack/wp-main.ts
+++ src/ts/webpack/wp-main.ts
@@ -1,7 +1,6 @@
 import { hideNoJs } from "@wp/main/example/hideNoJs"
 import { makeUrl } from "@wp/main/example/makeUrl"
 
-export function main(): void {
-  hideNoJs()
-  console.log("makeUrl", makeUrl("/aa/abcdefghi"))
-}
+console.log("webpack main")
+hideNoJs()
+console.log("makeUrl", makeUrl("/aa/abcdefghi"))
