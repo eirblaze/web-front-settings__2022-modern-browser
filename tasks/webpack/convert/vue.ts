e5bca8c5c53fd9a4b8564cd967a4c0f96a77b80a
eirblaze <eirblaze@live.jp>
2022-12-08 17:49:19 +0900

    キーを変数にする

--- tasks/webpack/convert/vue.ts
+++ tasks/webpack/convert/vue.ts
@@ -6,8 +6,6 @@ import { isDev } from "#tsk/getEnv"
 // const vue_import = 'vue' // CDN
 // const vue_import = 'vue/dist/vue.runtime.esm-browser' // ES Module (バンドラ用) // vue-loader や vueify を利用する場合、 *.vue ファイルに中のテンプレートはビルド時に JavaScript に事前コンパイルされます。最終成果物の中にコンパイラは本当に必要なく、したがってランタイム限定ビルドを利用することが出来ます。ランタイム限定ビルドは完全ビルドに比べおよそ 30% 軽量なため、利用できるときにはこれを利用したほうが良いでしょう。それでもなお完全ビルドを利用したい場合は、バンドラでエイリアスを設定する必要があります。
 const vue_import = isDev ? 'vue/dist/vue.runtime.esm-browser' : 'vue'
-const externalsOption = []
-externalsOption[vue_import] = 'Vue'
 
 const cfg: WpCfg = {
   module: {
@@ -35,7 +33,9 @@ const cfg: WpCfg = {
     extensions: ['.vue']
   },
   externals: [
-    externalsOption as any,
+    {
+      [vue_import]: 'Vue',
+    },
   ],
   plugins: [
     new VueLoaderPlugin(),
