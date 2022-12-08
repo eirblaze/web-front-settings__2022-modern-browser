e9d828e1fd36a5504a361a121b4bfdfeac071fb0
eirblaze <eirblaze@live.jp>
2022-12-08 17:42:06 +0900

    最新版だとインポートの方法が異なる

--- src/ts/webpack/wp-main.ts
+++ src/ts/webpack/wp-main.ts
@@ -1,6 +1,15 @@
 import { hideNoJs } from "@wp/main/example/hideNoJs"
 import { makeUrl } from "@wp/main/example/makeUrl"
+import Vue from 'vue/dist/vue.runtime.esm-browser'
 
 console.log("webpack main")
 hideNoJs()
 console.log("makeUrl", makeUrl("/aa/abcdefghi"))
+
+// 型推論を有効にする
+const Component = Vue.extend({
+  el: '#app-contents-index-menu',
+  render: (createElement) => createElement(VueTopIndex),
+  components: { VueTopIndex }
+})
+
