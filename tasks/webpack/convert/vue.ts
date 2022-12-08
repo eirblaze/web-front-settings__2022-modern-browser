bd37c83d85e86ff13c6111bfc9d3896e0b6d5137
eirblaze <eirblaze@live.jp>
2022-12-08 17:41:54 +0900

    external

--- tasks/webpack/convert/vue.ts
+++ tasks/webpack/convert/vue.ts
@@ -1,9 +1,14 @@
 import { Configuration as WpCfg } from "webpack"
 const { VueLoaderPlugin } = require('vue-loader')
+import { isDev } from "#tsk/getEnv"
 
 // Vue.js
-const vue_import = 'vue' // CDN
-// const vue_import = 'vue/dist/vue.esm.js' // ES Module (バンドラ用) // vue-loader や vueify を利用する場合、 *.vue ファイルに中のテンプレートはビルド時に JavaScript に事前コンパイルされます。最終成果物の中にコンパイラは本当に必要なく、したがってランタイム限定ビルドを利用することが出来ます。ランタイム限定ビルドは完全ビルドに比べおよそ 30% 軽量なため、利用できるときにはこれを利用したほうが良いでしょう。それでもなお完全ビルドを利用したい場合は、バンドラでエイリアスを設定する必要があります。
+// const vue_import = 'vue' // CDN
+// const vue_import = 'vue/dist/vue.runtime.esm-browser' // ES Module (バンドラ用) // vue-loader や vueify を利用する場合、 *.vue ファイルに中のテンプレートはビルド時に JavaScript に事前コンパイルされます。最終成果物の中にコンパイラは本当に必要なく、したがってランタイム限定ビルドを利用することが出来ます。ランタイム限定ビルドは完全ビルドに比べおよそ 30% 軽量なため、利用できるときにはこれを利用したほうが良いでしょう。それでもなお完全ビルドを利用したい場合は、バンドラでエイリアスを設定する必要があります。
+const vue_import = isDev ? 'vue/dist/vue.runtime.esm-browser' : 'vue'
+const externalsOption = []
+externalsOption[vue_import] = 'Vue'
+
 const cfg: WpCfg = {
   module: {
     rules: [
@@ -29,11 +34,9 @@ const cfg: WpCfg = {
     // in webpack 2.2 default resolve .js .json - https://github.com/vuejs/vue-loader/issues/685
     extensions: ['.vue']
   },
-  // externals: [
-  //   {
-  //     'vue/dist/vue.esm.js': 'Vue'
-  //   }
-  // ],
+  externals: [
+    externalsOption as any,
+  ],
   plugins: [
     new VueLoaderPlugin(),
   ],
