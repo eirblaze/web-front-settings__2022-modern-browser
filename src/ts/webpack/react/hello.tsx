633fec84f9a60c42f16c598170c4903e942e9a66
eirblaze <eirblaze@live.jp>
2022-12-12 18:37:41 +0900

    ホバーイベント試作

--- src/ts/webpack/react/hello.tsx
+++ src/ts/webpack/react/hello.tsx
@@ -4,13 +4,27 @@ export interface UserListProp {
   userList: string[]
 }
 
+const handleMouseEnter:React.MouseEventHandler = e => {
+  $(e.currentTarget).css({
+    "backgroundColor": "maroon",
+  })
+}
+const handleMouseLeave:React.MouseEventHandler = e => {
+  $(e.currentTarget).css({"backgroundColor": "transparent"})
+}
+
 const Users: React.FC<UserListProp> = (props) => <ul>
   {
-    props.userList.map( iterator => <li>{iterator}</li> )
+    props.userList.map( (iterator,index) => <li
+      key={index.toString()}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >{iterator}</li> )
   }
 </ul>
 
 function MyApp() {
+
   return <div>
     <h2>hello</h2>
     <Users userList={[
