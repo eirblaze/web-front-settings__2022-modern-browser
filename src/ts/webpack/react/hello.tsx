c95521a62f3b3205dd2159c63ff03c29f0ef6ed4
eirblaze <eirblaze@live.jp>
2022-12-12 18:02:14 +0900

    反復、プロパティ拡張

--- src/ts/webpack/react/hello.tsx
+++ src/ts/webpack/react/hello.tsx
@@ -1,9 +1,26 @@
 import React from "react"
 
-const name = 'my friend'
+export interface UserListProp {
+  userList: string[]
+}
+
+const Users: React.FC<UserListProp> = (props) => <ul>
+  {
+    props.userList.map( iterator => <li>{iterator}</li> )
+  }
+</ul>
 
 function MyApp() {
-  return <p>Hello, {name}</p>
+  return <div>
+    <h2>hello</h2>
+    <Users userList={[
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+    ]}/>
+  </div>
 }
 
 export const element = <MyApp />
